527065e7883939c2c746a04a1a012f6aef27d265
Liam Magee <liam.magee@gmail.com>
2013-12-20 18:09:29 +1100

    add tour skeleton, and other ui tweaks

---
+++ app/controllers/TourController.java
@@ -0,0 +1,28 @@
+package controllers;
+
+import models.*;
+import play.mvc.Result;
+import play.*;
+import play.data.Form;
+import play.mvc.*;
+
+import init.BasicInit;
+
+import views.html.*;
+import views.html.index;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: liam
+ * Date: 15/11/2013
+ * Time: 5:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TourController extends Controller {
+
+    public static Result index() {
+        return ok(
+            views.html.tour.index.render()
+        );
+    }
+}
